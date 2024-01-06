51bcb8b7aa9a7864cdb5d7ea45c1d23bc789c476
DanSteinmann <dan.steinmann@gmail.com>
2024-01-06 11:17:05 +0100

    solution 8

--- app/helpers/todos_helper.rb
+++ app/helpers/todos_helper.rb
@@ -1,6 +1,5 @@
 module TodosHelper
   def filter_class(filter)
-    # TODO, hint use `params[:scope]` to determine if the `selected` class should be added.
-    # voir corrigé
+    'selected' if params[:scope] == filter
   end
 end
